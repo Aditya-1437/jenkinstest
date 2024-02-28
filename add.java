c6ac32d66b781dd5e72d1f13821e04e8329778be
Aditya.K <kunchaaditya369@gmail.com>
2024-02-28 16:56:25 +0530

    This is a java code for add

---
+++ add.java
@@ -0,0 +1,12 @@
+class Main{
+	public static int add(int a, int b){
+		reutrn a+b;
+	}
+
+
+	public static void main(String[] args){
+		int a=20, b=30, c;
+		c=add(a,b);
+		System.out.println(c);
+	}
+}
