29b8596c836e61c9e6838faf46369440e8433920
Aditya-1437 <159253199+Aditya-1437@users.noreply.github.com>
2024-02-28 17:12:11 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,12 +1,12 @@
 public class add{
-	// public static int add(int a, int b){
-	// 	reutrn a+b;
-	// }
+	public static int add(int a, int b){
+		reutrn a + b;
+	}
 
 
 	public static void main(String[] args){
 		int a=20, b=30, c;
-		c=a+b;
+		c=add(a,b);
 		System.out.println(c);
 	}
 }
